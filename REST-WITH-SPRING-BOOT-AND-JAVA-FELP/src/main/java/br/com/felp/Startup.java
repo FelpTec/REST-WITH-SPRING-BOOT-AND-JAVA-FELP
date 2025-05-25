72c263287462d3b17d3060b1e5231b6d1657895e
Fellipe Timbó Martins Rodrigues <163943345+FelpTec@users.noreply.github.com>
2025-05-24 21:28:05 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ REST-WITH-SPRING-BOOT-AND-JAVA-FELP/src/main/java/br/com/felp/Startup.java
@@ -0,0 +1,13 @@
+package br.com.felp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
