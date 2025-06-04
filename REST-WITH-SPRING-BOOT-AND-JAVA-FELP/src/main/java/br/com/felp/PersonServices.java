6030f836c1c6c5d9d0bcd13123970ae085a8c730
Fellipe Timbó Martins Rodrigues <163943345+FelpTec@users.noreply.github.com>
2025-06-03 22:10:32 -0300

    Postman e Verbos HTTP, POST, PUT e DELETE

--- REST-WITH-SPRING-BOOT-AND-JAVA-FELP/src/main/java/br/com/felp/PersonServices.java
+++ REST-WITH-SPRING-BOOT-AND-JAVA-FELP/src/main/java/br/com/felp/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.felp;
 import br.com.felp.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,27 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for (int i = 0; i <8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Address " + i);
+        person.setGender("Gender");
+        return person;
+    }
 
     public Person findById(String id) {
         logger.info("Find one Person!");
@@ -25,4 +48,24 @@ public class PersonServices {
 
         return person;
     }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Deleting one Person!");
+
+    }
+
+
+
+
 }
